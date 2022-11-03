8a3b8048552d8a52087b757823bc5f19bb5c9ebd
Tobbil <kysthalden@gmail.com>
2022-11-03 14:38:14 +0100

    Changed name: config_provider -> config_providers, moved provider classes to config_providers.py

--- pytest_practice/src/config/config.py
+++ pytest_practice/src/config/config.py
@@ -1,7 +1,5 @@
-import json
-import os
 from typing import Any
-# from src.config.config_provider import ConfigProvider
+from config_providers import BaseProviderClass, OSConfigProvider, JSONConfigProvider
 
 
 class Config:
@@ -12,7 +10,6 @@ class Config:
         self.conf_dict = {}
         self._register("BASE_URL")
         self._register("SQL_CONNECTION_STRING")
-        self._register("SQL_CONNECTION_STRING2")
 
     def get(self, item_name: str) -> Any:
         return self.conf_dict[item_name]
@@ -27,33 +24,5 @@ class Config:
         raise ValueError(f"{item_name} name is missing in config")
 
 
-class BaseProviderClass:
-    @staticmethod
-    def get(item_name: str) -> Any:
-        raise NotImplementedError("Not implemented")
-
-
-class OSConfigProvider(BaseProviderClass):
-
-    @staticmethod
-    def get(item_name: str) -> Any:
-        value = os.getenv(item_name)
-        return value
-
-
-class JSONConfigProvider(BaseProviderClass):
-
-    @staticmethod
-    def _read_config(config_path):
-        with open(config_path) as json_file:
-            return json.load(json_file)
-
-    @staticmethod
-    def get(item_name: str) -> Any:
-        value = JSONConfigProvider._read_config(
-            "/home/tobiasz/talent-engine/pytest_practice/envs_configs/dev.json")
-        return value[item_name]
-
-
 config = Config([OSConfigProvider, JSONConfigProvider])
-print(config.get['SQL_CONNECTION_STRING'])
+print(config.get("SQL_CONNECTION_STRING"))
