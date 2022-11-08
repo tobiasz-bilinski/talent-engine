205c3089bd6b3aaf564e10d045ba1a720852de31
Tobbil <kysthalden@gmail.com>
2022-11-08 20:58:51 +0100

    Expanded docstrings. Config: changed get to __getattr__. Split providers into different modules.

---
+++ framework/src/config/os_config_provider.py
@@ -0,0 +1,17 @@
+import os
+from typing import Any
+from base_provider import BaseProviderClass
+
+
+class OSConfigProvider(BaseProviderClass):
+    def __getitem__(self, item_name: str) -> Any:
+        """Returns value of environment variable item_name or None if it doesn't exist.
+        
+        Args:
+            item_name (str): Item to get from configuration
+
+        Returns:
+            Any: Value of item in configuration
+        """
+        value = os.getenv(item_name)
+        return value
