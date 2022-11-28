1b42eb4cdcadc456d7c0e46f93ebdc25c1466f87
tobiasz-bilinski <tobiasz.bilinski@globallogic.com>
2022-11-28 12:37:46 +0100

    Cosmetic changes

--- src/data/test_data.py
+++ src/data/test_data.py
@@ -1,22 +1,26 @@
 class TestData:
     # DATA TYPES TESTS
-    test_data_types_positive = [(1, int),
-                                (2.5, float),
-                                ("String", str),
-                                (False, bool),
-                                (["a", "b", "c"], list),
-                                ({"a": 1, "b": 2}, dict),
-                                ((1, 2, 3), tuple),
-                                ({1, 2, 3}, set)]
+    test_data_types_positive = [
+        (1, int),
+        (2.5, float),
+        ("String", str),
+        (False, bool),
+        (["a", "b", "c"], list),
+        ({"a": 1, "b": 2}, dict),
+        ((1, 2, 3), tuple),
+        ({1, 2, 3}, set),
+    ]
 
-    test_data_types_negative = [("1", int),
-                                (2, float),
-                                (True, str),
-                                ([1, 2], bool),
-                                ({"a", "b", "c"}, list),
-                                ([1, 2, 3], dict),
-                                ({"a": 1, "b": 2}, tuple),
-                                ([1, 2, 3], set)]
+    test_data_types_negative = [
+        ("1", int),
+        (2, float),
+        (True, str),
+        ([1, 2], bool),
+        ({"a", "b", "c"}, list),
+        ([1, 2, 3], dict),
+        ({"a": 1, "b": 2}, tuple),
+        ([1, 2, 3], set),
+    ]
     # USER TESTS
     USERNAME = "Tobiasz"
     BIRTH_YEAR = 1990
--- src/pages/forgot_password_page.py
+++ src/pages/forgot_password_page.py
@@ -5,7 +5,6 @@ from src.config.config import config
 class ForgotPasswordPage:
     PAGE_URL = "/password_reset"
     email_field = (By.ID, "email_field")
-    verification_button = (By.ID, "home_children_button")
     submit_button = (By.NAME, "commit")
 
     def __init__(self, app):
@@ -14,16 +13,12 @@ class ForgotPasswordPage:
     def open_forgot_password_page(self):
         self.app.open_page(config.BASE_URL_UI + self.PAGE_URL)
 
-    def fill_and_submit(self, email):
-        """Fill out email field, click verify button, click submit button.
+    def fill_email(self, email):
+        """Fill out email field.
 
         Args:
             email (str): Shared email stored in config.
 
         """
-        # Just an example, because it's a captcha so it doesn't make much sense
-        # Also I'm not sure how to make Selenium wait for the button to appear
         self.app.enter_text(self.email_field, email)
-        self.app.click(self.verification_button)
-        self.app.click(self.submit_button)
 
